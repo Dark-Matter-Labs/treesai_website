6f2d97ebe117276da87acad429c88aefac52e038
Gurden Batra <gurden.batra@gmail.com>
2021-07-19 13:06:30 +0300

    Updating social proof section with

--- src/components/SocialProof.js
+++ src/components/SocialProof.js
@@ -1,15 +1,11 @@
+import googleLogo from "../images/google.org_logo.png";
+import glasgowLogo from "../images/glasgowlogo.jpeg";
+
 export default function SocialProof() {
   return (
     <section className="bg-indigo-800">
       <div className="max-w-7xl mx-auto md:grid md:grid-cols-2 md:px-6 lg:px-8">
         <div className="py-12 px-4 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10 md:border-r md:border-indigo-900 lg:pr-16">
-          <div className="md:flex-shrink-0">
-            <img
-              className="h-12"
-              src="https://tailwindui.com/img/logos/tuple-logo-indigo-300.svg"
-              alt="Tuple"
-            />
-          </div>
           <blockquote className="mt-6 md:flex-grow md:flex md:flex-col">
             <div className="relative text-lg font-medium text-white md:flex-grow">
               <svg
@@ -30,21 +26,14 @@ export default function SocialProof() {
                 long-term maintenance of urban forests.
               </p>
             </div>
+            <div className="md:flex-shrink-0">
+              <img className="h-12" src={googleLogo} alt="Google.org logo" />
+            </div>
             <footer className="mt-8">
               <div className="flex items-start">
-                <div className="flex-shrink-0 inline-flex rounded-full border-2 border-white">
-                  <img
-                    className="h-12 w-12 rounded-full"
-                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </div>
-                <div className="ml-4">
+                <div>
                   <div className="text-base font-medium text-white">
-                    Kirsten Dunlop
-                  </div>
-                  <div className="text-base font-medium text-indigo-200">
-                    CEO, EIT Climate-KIC
+                    Google.org
                   </div>
                 </div>
               </div>
@@ -52,13 +41,6 @@ export default function SocialProof() {
           </blockquote>
         </div>
         <div className="py-12 px-4 border-t-2 border-indigo-900 sm:px-6 md:py-16 md:pr-0 md:pl-10 md:border-t-0 md:border-l lg:pl-16">
-          <div className="md:flex-shrink-0">
-            <img
-              className="h-12"
-              src="https://tailwindui.com/img/logos/workcation-logo-indigo-300.svg"
-              alt="Workcation"
-            />
-          </div>
           <blockquote className="mt-6 md:flex-grow md:flex md:flex-col">
             <div className="relative text-lg font-medium text-white md:flex-grow">
               <svg
@@ -69,27 +51,37 @@ export default function SocialProof() {
                 <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
               </svg>
               <p className="relative">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-                expedita voluptas culpa sapiente alias molestiae. Numquam
-                corrupti in laborum sed rerum et corporis. Nemo expedita
-                voluptas culpa sapiente alias molestiae.
+                So many of the challenges surrounding the climate emergency will
+                be addressed by our cities and Nature-based Solutions are
+                absolutely critical within that. Urban forests deliver
+                significant ecological, environmental and social gains, creating
+                safer, more resilient and more liveable places. The road to Net
+                Zero must also be delivered in a way that resonates with
+                ordinary citizens and is relevant to their everyday lives. The
+                transformation of the look and feel of our communities whilst
+                addressing greenhouse gases does just that. Our status as COP
+                host has given us a tremendous opportunity to attract some of
+                the most pioneering approaches and interventions within the
+                climate agenda. Our selection as a location for one of the
+                projects supported by the Google.org Impact Challenge on Climate
+                underlines that.
               </p>
             </div>
             <footer className="mt-8">
+              <div className="md:flex-shrink-0">
+                <img
+                  className="h-28"
+                  src={glasgowLogo}
+                  alt="City of Glasgow logo"
+                />
+              </div>
               <div className="flex items-start">
-                <div className="flex-shrink-0 inline-flex rounded-full border-2 border-white">
-                  <img
-                    className="h-12 w-12 rounded-full"
-                    src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                </div>
-                <div className="ml-4">
+                <div>
                   <div className="text-base font-medium text-white">
-                    Joseph Rodriguez
+                    Susan Aitken
                   </div>
                   <div className="text-base font-medium text-indigo-200">
-                    CEO, Workcation
+                    CEO of Glasgow City Council
                   </div>
                 </div>
               </div>
