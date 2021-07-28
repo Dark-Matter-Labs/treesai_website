8da872b851e62f53f6c6ca000474516d9611db98
Gurden Batra <gurden.batra@gmail.com>
2021-07-28 21:30:03 +0300

    Adding background image and content update

--- src/components/Vision.js
+++ src/components/Vision.js
@@ -1,15 +1,13 @@
-import "../App.css";
-
 export default function Vision() {
   return (
-    <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
+    <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden section-bg">
       <div className="mx-auto max-w-7xl lg:px-8">
         <div className="lg:grid lg:grid-cols-2 lg:gap-8">
           <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
             <div className="lg:py-24">
               <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
                 <span className="block">
-                  A platform to value, and invest in nature beyond carbon
+                  A platform to value, and invest in nature, beyond carbon
                 </span>
                 <span className="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-cyan-400 sm:pb-5">
                   ship web apps
@@ -45,20 +43,16 @@ export default function Vision() {
                   </div>
                   <p className="mt-3 text-sm text-gray-300 sm:mt-4">
                     We hate spam too. By providing your email, you agree to our{" "}
-                    <a href="https://treesai.netlify.app/" className="font-medium text-white">
+                    <a
+                      href="https://treesai.netlify.app/"
+                      className="font-medium text-white"
+                    >
                       terms or service
                     </a>
                     .
                   </p>
                 </form>
               </div>
-            </div>
-          </div>
-          <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
-            <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-              <video playsInline autoPlay muted loop id="bgvid">
-                <source src="back.mp4" type="video/mp4" />
-              </video>
             </div>
           </div>
         </div>
