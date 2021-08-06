56e9941ef1c08184af0583aa888ea98bf5dcf172
Gurden Batra <gurden.batra@gmail.com>
2021-08-06 11:37:03 +0300

    Commit to test out mailchimp embed

--- src/components/Vision.js
+++ src/components/Vision.js
@@ -19,23 +19,27 @@ export default function Vision() {
                 investment, profitability and sustainability.
               </p>
               <div className="mt-10 sm:mt-12">
-                <form action="#" className="sm:max-w-xl sm:mx-auto lg:mx-0">
+                <form action="https://treesasinfrastructure.us5.list-manage.com/subscribe/post?u=79b921b3858895873082cb61b&amp;id=5861f9779c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate className="sm:max-w-xl sm:mx-auto lg:mx-0 validate">
                   <div className="sm:flex">
                     <div className="min-w-0 flex-1">
                       <label htmlFor="email" className="sr-only">
                         Email address
                       </label>
                       <input
-                        id="email"
+                        id="mce-EMAIL"
                         type="email"
+                        required
                         placeholder="Enter your email"
-                        className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+                        className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900 email"
                       />
                     </div>
                     <div className="mt-3 sm:mt-0 sm:ml-3">
                       <button
                         type="submit"
-                        className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+                        value="Subscribe" 
+                        name="subscribe" 
+                        id="mc-embedded-subscribe"
+                        className="button block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
                       >
                         Get notified when we launch
                       </button>
