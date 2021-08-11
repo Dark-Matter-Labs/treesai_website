91909d7ce8cbf35ca7c7c5f4e111eba4932e9f29
Gurden Batra <gurden.batra@gmail.com>
2021-08-11 12:49:42 +0300

    refactoring code for Mailchimp form

--- src/components/Vision.js
+++ src/components/Vision.js
@@ -1,8 +1,8 @@
-import MailchimpSubscribe from "react-mailchimp-subscribe"
+import MailchimpSubscribe from "react-mailchimp-subscribe";
 import CustomForm from "./SignUpForm";
 
 const url =
-      "https://treesasinfrastructure.us5.list-manage.com/subscribe/post?u=79b921b3858895873082cb61b&amp;id=5861f9779c";
+  "https://treesasinfrastructure.us5.list-manage.com/subscribe/post?u=79b921b3858895873082cb61b&amp;id=5861f9779c";
 
 export default function Vision() {
   return (
@@ -25,49 +25,16 @@ export default function Vision() {
                 investment, profitability and sustainability.
               </p>
               <div className="mt-10 sm:mt-12">
-                <form action="#" className="sm:max-w-xl sm:mx-auto lg:mx-0">
-                  <div className="sm:flex">
-                    <div className="min-w-0 flex-1">
-                    <MailchimpSubscribe
-                      url={url}
-                      render={({ subscribe, status, message }) => (
-                        <CustomForm
-                          status={status}
-                          message={message}
-                          onValidated={formData => subscribe(formData)}
-                        />
-                      )}
+                <MailchimpSubscribe
+                  url={url}
+                  render={({ subscribe, status, message }) => (
+                    <CustomForm
+                      status={status}
+                      message={message}
+                      onValidated={(formData) => subscribe(formData)}
                     />
-                      <label htmlFor="email" className="sr-only">
-                        Email address
-                      </label>
-                      <input
-                        id="email"
-                        type="email"
-                        placeholder="Enter your email"
-                        className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
-                      />
-                    </div>
-                    <div className="mt-3 sm:mt-0 sm:ml-3">
-                      <button
-                        type="submit"
-                        className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
-                      >
-                        Get notified when we launch
-                      </button>
-                    </div>
-                  </div>
-                  <p className="mt-3 text-sm text-gray-300 sm:mt-4">
-                    We hate spam too. By providing your email, you agree to our{" "}
-                    <a
-                      href="https://treesai.netlify.app/"
-                      className="font-medium text-white"
-                    >
-                      terms or service
-                    </a>
-                    .
-                  </p>
-                </form>
+                  )}
+                />
               </div>
             </div>
           </div>
