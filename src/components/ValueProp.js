ee5d1219d401ba7670179b2b4dca2a1a4795f9b9
Gurden Batra <gurden.batra@gmail.com>
2021-08-18 17:54:19 +0300

    Change layout

--- src/components/ValueProp.js
+++ src/components/ValueProp.js
@@ -2,7 +2,7 @@ export default function ValueProp() {
   const logos = [
     { name: "Google.org", url: "images/partner_logos/google.org_logo.svg" },
     { name: "Morgan Stanley", url: "images/partner_logos/Morgan_Stanley.svg" },
-    { name: "NatureScot", url: "images/partner_logos/NatureScot.svg" },
+    { name: "NatureScot", url: "images/partner_logos/NatureScot.png" },
     {
       name: "World Economic Forum",
       url: "images/partner_logos/world_economic_forum.svg",
@@ -12,19 +12,15 @@ export default function ValueProp() {
 
   return (
     <div className="bg-light bg-opacity-25">
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <div className="lg:grid lg:grid-cols-2 lg:gap-8">
-          <h2 className="max-w-md mx-auto text-3xl font-bold text-primary text-center lg:max-w-xl lg:text-left">
-            Trusted by government agencies, supported by technology and finance
-            leaders
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-2">
+        <div className="">
+          <h2 className="max-w-md text-3xl font-bold text-primary text-center lg:max-w-xl lg:text-left">
+            Supported by
           </h2>
-          <div className="mt-12 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
+          <div className="mt-12 grid grid-cols-5 gap-1 md:grid-cols-3 lg:mt-0 lg:grid-cols-5 justify-center">
             {logos.map((logo) => (
-              <div
-                key={logo.name}
-                className="col-span-1 flex justify-center py-8 px-8 bg-white"
-              >
-                <img className="max-h-12" src={logo.url} alt={logo.name} />
+              <div key={logo.name} className="flex py-8 px-4 bg-transparent">
+                <img className="" src={logo.url} alt={logo.name} />
               </div>
             ))}
           </div>
