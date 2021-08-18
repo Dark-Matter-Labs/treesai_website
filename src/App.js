f747c0e18fdf2c69c87e13084fc106fba50b3c76
Gurden Batra <gurden.batra@gmail.com>
2021-08-18 19:46:45 +0300

    Part of homepage refactor and adding router links

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import Vision from "./components/Vision";
-import ValueProp from "./components/ValueProp";
-import Features from "./components/Features";
-import SocialProof from "./components/SocialProof";
-import Pilot from "./components/Pilot";
-import Team from "./components/Team";
-import Reports from "./components/Reports";
-import GetInTouch from "./components/GetInTouch";
-import Footer from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import ScrollToTop from "./components/ScrollToTop";
 
 import "./App.css";
 
 export default function TreesAIWebsite() {
   return (
-    <div className="bg-white">
-      <div className="relative overflow-hidden">
-        <main>
-          <Vision />
-          <ValueProp />
-          <Features />
-          <SocialProof />
-          <Pilot />
-          <Team />
-          <Reports />
-          <GetInTouch />
-        </main>
-        <Footer />
-      </div>
-    </div>
+    <Router>
+      <ScrollToTop />
+      <Switch>
+        <Route path="/privacy">
+          <PrivacyPolicy />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
