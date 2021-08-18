79f322a4e2208da61b564b47711ffa03f50eb547
Gurden Batra <gurden.batra@gmail.com>
2021-08-18 19:45:38 +0300

    Create page for privacy policy

---
+++ src/pages/PrivacyPolicy.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+import Privacy from "../components/Privacy";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <div className="bg-white">
+      <div className="relative overflow-hidden">
+        <div className="mt-10 space-x-8 md:flex md:ml-10">
+          <Link
+            to="/"
+            className="text-base font-medium text-dark hover:text-gray-300 pointer-cursor"
+          >
+            ← Back
+          </Link>
+        </div>
+        <Privacy />
+        <Footer />
+      </div>
+    </div>
+  );
+}
