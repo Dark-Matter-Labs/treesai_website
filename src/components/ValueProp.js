71aae4f9fd3f72d7d604889f06a21ddbdeca301a
Gurden Batra <gurden.batra@gmail.com>
2021-08-19 22:08:58 +0300

    update icons

--- src/components/ValueProp.js
+++ src/components/ValueProp.js
@@ -1,23 +1,25 @@
 export default function ValueProp() {
   const logos = [
-    { name: "Google.org", url: "images/partner_logos/google.org_logo.svg" },
-    { name: "Morgan Stanley", url: "images/partner_logos/Morgan_Stanley.svg" },
+    { name: "EIT Climate-KIC", url: "images/partner_logos/eit-climatekic.svg" },
     { name: "NatureScot", url: "images/partner_logos/NatureScot.svg" },
+    { name: "Morgan Stanley", url: "images/partner_logos/Morgan_Stanley.svg" },
     {
       name: "World Economic Forum",
       url: "images/partner_logos/world_economic_forum.svg",
     },
-    { name: "EIT Climate-KIC", url: "images/partner_logos/eit-climatekic.svg" },
+    { name: "Google.org", url: "images/partner_logos/google.org_logo.svg" },
   ];
 
   return (
-    <div className="bg-light bg-opacity-25">
+    <div className="bg-gray">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-2">
         <div className="">
-          <h2 className="max-w-md text-3xl font-bold text-primary text-center lg:max-w-xl lg:text-left">
-            Supported by
-          </h2>
-          <div className="mt-12 grid grid-cols-5 gap-1 md:grid-cols-3 lg:mt-0 lg:grid-cols-5 justify-center grid-center">
+          <div className="text-center mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+            <p className="mt-2 text-3xl font-bold text-gray-900 tracking-tight sm:text-4xl">
+              Supported by
+            </p>
+          </div>
+          <div className="mt-12 grid grid-cols-5 gap-12 sm:grid-cols-1 md:grid-cols-3 lg:mt-0 lg:grid-cols-5 justify-center grid-center">
             {logos.map((logo) => (
               <div key={logo.name} className="flex py-8 px-4 bg-transparent">
                 <img className="" src={logo.url} alt={logo.name} />
