a04ffb932033bef28be4289509d346279a4ad95c
Gurden Batra <gurden.batra@gmail.com>
2021-08-19 22:11:50 +0300

    colour and layout update

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -2,46 +2,49 @@ import { MailIcon } from "@heroicons/react/outline";
 
 export default function GetInTouch() {
   return (
-    <div className="relative bg-white" id="contact">
-      <div className="absolute inset-0">
-        <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50" />
-      </div>
-      <div className="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5">
-        <div className="bg-gray-50 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
+    <div className="relative bg-gray" id="contact">
+      <div className="relative max-w-7xl mx-auto md:grid-cols-2 lg:grid lg:grid-cols-2 sm:grid-cols-1">
+        <div className="bg-gray-50 py-16 px-4">
           <div className="max-w-lg mx-auto">
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               Get in touch
             </h2>
-            <p className="mt-3 text-lg font-semibold leading-6 text-gray-500 font-body">
-              Are you a municipality?
-            </p>
-            <p className="text-lg leading-6 text-gray-500 font-body">
-              TreesAI can help solve your urban infrastructure issues.
-            </p>
-            <p className="mt-3 text-lg font-semibold leading-6 text-gray-500 font-body">
-              Do you have talent or resources to help us?
-            </p>
-            <p className="text-lg leading-6 text-gray-500 font-body">
-              Become a partner of TreesAI.
-            </p>
+            <div className="pt-2 pb-2">
+              <p className="mt-3 text-lg font-semibold leading-6 text-gray-500">
+                Are you a municipality?
+              </p>
+              <p className="text-lg leading-6 text-dark">
+                TreesAI can help solve your urban infrastructure issues.
+              </p>
+            </div>
+            <div className="pt-2 pb-2">
+              <p className="mt-3 text-lg font-semibold leading-6 text-gray-500">
+                Do you have talent or resources to help us?
+              </p>
+              <p className="text-lg leading-6 text-dark">
+                Become a partner of TreesAI.
+              </p>
+            </div>
 
-            <p className="mt-3 text-lg font-semibold leading-6 text-gray-500 font-body">
-              Are you a beneficiary of green infrastructure?
-            </p>
-            <p className="text-lg leading-6 text-gray-500 font-body">
-              We are currently developing the tools to help you invest in trees
-              as assets and can answer your questions.
-            </p>
+            <div className="pt-2 pb-2">
+              <p className="mt-3 text-lg font-semibold leading-6 text-gray-500">
+                Are you a beneficiary of green infrastructure?
+              </p>
+              <p className="text-lg leading-6 text-dark">
+                We are currently developing the tools to help you invest in
+                trees as assets and can answer your questions.
+              </p>
+            </div>
 
             <dl className="mt-8 text-base text-gray-500">
               <div className="mt-3">
                 <dt className="sr-only">Email</dt>
                 <dd className="flex">
                   <MailIcon
-                    className="flex-shrink-0 h-6 w-6 text-primary"
+                    className="flex-shrink-0 h-6 w-6 text-dark"
                     aria-hidden="true"
                   />
-                  <span className="ml-3 text-primary font-bold">
+                  <span className="ml-3 text-dark font-semibold">
                     <a href="mailto:treesai@darkmatterlabs.org">
                       treesai@darkmatterlabs.org
                     </a>
@@ -49,17 +52,10 @@ export default function GetInTouch() {
                 </dd>
               </div>
             </dl>
-            <dl className="mt-8 text-base">
-              <a
-                href="http://eepurl.com/hFsY1n"
-                target="_blank"
-                rel="noreferrer noopener"
-                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-primary hover:bg-gray-700"
-              >
-                Get notified when we launch
-              </a>
-            </dl>
           </div>
+        </div>
+        <div className="lets-invest">
+          <img src="../images/letsinvest.jpeg" alt=""></img>
         </div>
       </div>
     </div>
