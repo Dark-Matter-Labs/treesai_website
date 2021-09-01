1795443582f2bf3ef26ff421afa61bda1427365b
Gurden Batra <gurden.batra@gmail.com>
2021-09-01 14:21:13 +0300

    Adding custom icons for Features

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,42 +1,40 @@
-import {
-  ShieldCheckIcon,
-  CashIcon,
-  ChartPieIcon,
-  GlobeIcon,
-  ChartBarIcon,
-  LibraryIcon,
-  SparklesIcon,
-  GlobeAltIcon,
-} from "@heroicons/react/outline";
+import GlobalMarketIcon from "./icons/GlobalMarketsIcon";
+import SatelliteIcon from "./icons/SatelliteIcon";
+import CashFlowIcon from "./icons/CashFlowIcon";
+import GoalsIcon from "./icons/GoalsIcon";
+import CarbonCreditsIcon from "./icons/CarbonCreditsIcon";
+import AdvocateIcon from "./icons/AdvocateIcon";
+import FarmIcon from "./icons/FarmIcon";
+import SecurityIcon from "./icons/SecurityIcon";
 
 const councilFeatures = [
-  {
-    name: "Manage your natural assets",
-    plus: false,
-    description:
-      "Digital inventories provide information on the performance and state of urban trees. ",
-    icon: GlobeIcon,
-  },
   {
     name: "Attract stable, long-term finance",
     plus: false,
     description:
-      "Outcomes-based financial instruments based on environmental performance, generate revenue and support long-term ecological maintenance.",
-    icon: CashIcon,
+      "Digital inventories provide information on the performance and state of urban trees. ",
+    icon: GlobalMarketIcon,
   },
   {
-    name: "Plan and forecast climate targets",
+    name: "Manage your natural assets",
     plus: false,
     description:
-      "AI-backed modelling enables near real-time environmental impact assessments.",
-    icon: ChartBarIcon,
+      "Outcomes-based financial instruments based on environmental performance, generate revenue and support long-term ecological maintenance.",
+    icon: SatelliteIcon,
   },
   {
     name: "Reduce operational costs",
     plus: false,
     description:
+      "AI-backed modelling enables near real-time environmental impact assessments.",
+    icon: CashFlowIcon,
+  },
+  {
+    name: "Plan and forecast climate targets",
+    plus: false,
+    description:
       "Digital contracting mechanisms reduce cumbersome bureaucratic processes.",
-    icon: ChartPieIcon,
+    icon: GoalsIcon,
   },
 ];
 const investorFeatures = [
@@ -45,28 +43,28 @@ const investorFeatures = [
     plus: true,
     description:
       "are verified credits, linked to local projects and validated by city authorities.",
-    icon: LibraryIcon,
+    icon: CarbonCreditsIcon,
   },
   {
     name: "Make a local impact",
     plus: false,
     description:
       "Your investments achieve visibility by supporting local initiatives and communities.",
-    icon: GlobeAltIcon,
+    icon: AdvocateIcon,
   },
   {
     name: "Support multiple impacts",
     plus: true,
     description:
       "accounts for carbon-related benefits and estimates additional environmental impacts such as air quality improvements, water management, and human health benefits.",
-    icon: SparklesIcon,
+    icon: FarmIcon,
   },
   {
     name: "Transparent and verifiable",
     plus: false,
     description:
       "Trusted satellite and ground data map, monitor and account for urban tree outcomes.",
-    icon: ShieldCheckIcon,
+    icon: SecurityIcon,
   },
 ];
 
@@ -80,26 +78,28 @@ export default function Features() {
               <h2 className="text-base text-primary font-semibold tracking-wide uppercase">
                 FOR COUNCILS
               </h2>
-              <p className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl">
+              <p className="font-grotesk mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl">
                 Reach the climate targets for your city<br></br>
                 <br></br>
               </p>
             </div>
 
             <div className="mt-10">
-              <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+              <dl className="mt-10 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-8 sm:gap-y-10 lg:mt-0 lg:col-span-2">
                 {councilFeatures.map((feature) => (
-                  <div key={feature.name} className="relative">
+                  <div key={feature.name}>
                     <dt>
-                      <div className="absolute flex items-center justify-center h-12 w-12 md:h-10 md:w-10 rounded-md bg-primary text-white">
+                      <div className="flex items-center justify-center">
                         <feature.icon
-                          className="h-6 w-6 md:h-5 md:w-5"
+                          className="h-20 w-20"
                           aria-hidden="true"
                         />
                       </div>
-                      <p className="ml-16 lg:py-2 md:py-2 sm:py-2 text-lg leading-6 font-medium text-gray-900">
-                        {feature.name}
-                      </p>
+                      <div className="flex items-center justify-center text-center">
+                        <p className="mt-5 text-lg leading-6 font-medium text-gray-900">
+                          {feature.name}
+                        </p>
+                      </div>
                     </dt>
                   </div>
                 ))}
@@ -114,47 +114,31 @@ export default function Features() {
               <h2 className="text-base font-semibold tracking-wide uppercase text-blue">
                 FOR INVESTORS
               </h2>
-              <p className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl">
+              <p className="font-grotesk mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl">
                 Invest in government-backed carbon credits and support local
                 impacts
               </p>
             </div>
 
             <div className="mt-10">
-              <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-                {investorFeatures.map((feature) => {
-                  if (feature.plus)
-                    return (
-                      <div key={feature.name} className="relative">
-                        <dt>
-                          <div className="absolute flex items-center justify-center h-12 w-12 md:h-10 md:w-10 rounded-md bg-blue text-white">
-                            <feature.icon
-                              className="h-6 w-6 md:h-5 md:w-5"
-                              aria-hidden="true"
-                            />
-                          </div>
-                          <p className="ml-16 lg:py-2 md:py-2 sm:py-2 text-lg leading-6 font-medium text-gray-900">
-                            {feature.name}
-                          </p>
-                        </dt>
+              <dl className="mt-10 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-8 sm:gap-y-10 lg:mt-0 lg:col-span-2">
+                {investorFeatures.map((feature) => (
+                  <div key={feature.name}>
+                    <dt>
+                      <div className="flex items-center justify-center">
+                        <feature.icon
+                          className="h-20 w-20"
+                          aria-hidden="true"
+                        />
                       </div>
-                    );
-                  return (
-                    <div key={feature.name} className="relative">
-                      <dt>
-                        <div className="absolute flex items-center justify-center h-12 w-12 md:h-10 md:w-10 rounded-md bg-blue text-white">
-                          <feature.icon
-                            className="h-6 w-6 md:h-5 md:w-5"
-                            aria-hidden="true"
-                          />
-                        </div>
-                        <p className="ml-16 lg:py-3 md:py-2 sm:py-1 text-lg leading-6 font-medium text-gray-900">
+                      <div className="flex items-center justify-center text-center">
+                        <p className="mt-5 text-lg leading-6 font-medium text-gray-900">
                           {feature.name}
                         </p>
-                      </dt>
-                    </div>
-                  );
-                })}
+                      </div>
+                    </dt>
+                  </div>
+                ))}
               </dl>
             </div>
           </div>
