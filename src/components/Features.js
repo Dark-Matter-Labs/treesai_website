c234113068d01ec5bf3a26011b939cf249236eee
Gurden Batra <gurden.batra@gmail.com>
2021-09-20 13:49:57 +0300

    content from website

--- src/components/Features.js
+++ src/components/Features.js
@@ -76,10 +76,10 @@ export default function Features() {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="lg:text-center">
               <h2 className="text-base text-primary font-semibold tracking-wide uppercase">
-                FOR COUNCILS
+                FOR DEVELOPERS
               </h2>
               <p className="font-grotesk mt-2 text-3xl text-gray-900 sm:text-4xl">
-                Reach the climate targets for your city<br></br>
+              Fund your Nature-based Solutions projects<br></br>
                 <br></br>
               </p>
             </div>
@@ -115,8 +115,7 @@ export default function Features() {
                 FOR INVESTORS
               </h2>
               <p className="font-grotesk mt-2 text-3xl text-gray-900 sm:text-4xl">
-                Invest in government-backed carbon credits and support local
-                impacts
+                Invest in Nature to reduce your climate risks
               </p>
             </div>
 
