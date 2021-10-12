b48b32c9aa8dc3fa43ce9c8f5efe238a4f9fd0be
Gurden Batra <gurden.batra@gmail.com>
2021-10-12 21:19:57 +0300

    Adding page for demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import ImpactPlanner from "./pages/ImpactPlanner";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import ScrollToTop from "./components/ScrollToTop";
 
@@ -11,6 +12,9 @@ export default function TreesAIWebsite() {
     <Router>
       <ScrollToTop />
       <Switch>
+        <Route path="/impact-planner">
+          <ImpactPlanner />
+        </Route>
         <Route path="/privacy">
           <PrivacyPolicy />
         </Route>
