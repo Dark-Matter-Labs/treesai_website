8f7682b27505d3e3e298104f4c22a14b82cf8ab6
Gurden Batra <gurden.batra@gmail.com>
2021-10-25 17:54:42 +0300

    Adding map with city markers

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -1,4 +1,7 @@
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+
 import NavBar from "../components/demo/NavBar";
 
 const cities = [
@@ -47,10 +50,47 @@ const cities = [
 ];
 
 export default function Cities() {
+  mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng] = useState(1.4196);
+  const [lat] = useState(50.0489);
+  const [zoom] = useState(3);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/outdoors-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+
+    new mapboxgl.Marker().setLngLat([-4.2568, 55.8508]).addTo(map.current); //Glasgow
+    new mapboxgl.Marker({ color: "black" })
+      .setLngLat([-0.1276, 51.5072])
+      .addTo(map.current); //London
+    new mapboxgl.Marker({ color: "black" })
+      .setLngLat([-6.2603, 53.3498])
+      .addTo(map.current); //Dublin
+    new mapboxgl.Marker({ color: "black" })
+      .setLngLat([2.3522, 48.8566])
+      .addTo(map.current); //Paris
+    new mapboxgl.Marker({ color: "black" })
+      .setLngLat([9.19, 45.4642])
+      .addTo(map.current); //Milan
+    new mapboxgl.Marker({ color: "black" })
+      .setLngLat([13.405, 52.52])
+      .addTo(map.current); //Berlin
+
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <>
       <NavBar />
-      <div className="relative bg-gray py-16 sm:py-24 lg:py-20 px-16">
+      <div className="bg-gray py-16 sm:py-24 lg:py-20 px-16">
         <nav className="flex" aria-label="Breadcrumb">
           <ol className="flex items-center space-x-4">
             <li>
@@ -65,7 +105,7 @@ export default function Cities() {
             </li>
           </ol>
         </nav>
-        <div class="grid grid-cols-2 gap-4">
+        <div class="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4">
           <div className="mt-10">
             <h2 className="font-grotesk mt-2 text-3xl text-gray-900 sm:text-4xl">
               Find places to invest in nature
@@ -110,7 +150,9 @@ export default function Cities() {
               </div>
             </div>
           </div>
-          <div>map</div>
+          <div className="h-full w-full">
+            <div ref={mapContainer} className="map-container" />
+          </div>
         </div>
       </div>
     </>
