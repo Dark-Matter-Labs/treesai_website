095c0c31394203b4695dac33f94165f8bbc11947
Gurden Batra <gurden.batra@gmail.com>
2021-10-27 15:48:26 +0300

    Adding tailwind form support

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,5 +40,8 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+  ],
 };
