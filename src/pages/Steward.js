bb8e6f7c6529a81ee807ff7cab562d8db1dea81f
Gurden Batra <gurden.batra@gmail.com>
2021-11-02 15:44:28 +0200

    Update button copy

--- src/pages/Steward.js
+++ src/pages/Steward.js
@@ -1303,7 +1303,7 @@ export default function Steward() {
                         to="/invest-in-nature/glasgow"
                         className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                       >
-                        Invest in Glasgow
+                        Check out Glasgow portfolio
                       </Link>
                     </div>
                   </div>
