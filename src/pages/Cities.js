d6ebdd716272f48c8a7acd27806ac5e0df519823
Gurden Batra <gurden.batra@gmail.com>
2021-11-02 22:55:47 +0200

    Cleaning layout of Cities and Glasgow page

--- src/pages/Cities.js
+++ src/pages/Cities.js
@@ -5,6 +5,10 @@ import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-load
 import NavBar from "../components/demo/NavBar";
 import letsInvestImage from "../images/letsinvest.jpeg";
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 const cities = [
   {
     id: 1,
@@ -17,6 +21,7 @@ const cities = [
     id: 2,
     name: "Dublin",
     imageSrc: "assets/Dublin.png",
+    href: "",
     imageAlt: "Dublin city",
     progressText: "coming soon",
   },
@@ -24,6 +29,7 @@ const cities = [
     id: 3,
     name: "London",
     imageSrc: "assets/London.png",
+    href: "",
     imageAlt: "London city",
     progressText: "coming soon",
   },
@@ -31,6 +37,7 @@ const cities = [
     id: 4,
     name: "Paris",
     imageSrc: "assets/Paris.png",
+    href: "",
     imageAlt: "Paris city",
     progressText: "coming soon",
   },
@@ -38,6 +45,7 @@ const cities = [
     id: 5,
     name: "Milan",
     imageSrc: "assets/Milan.png",
+    href: "",
     imageAlt: "Milan city",
     progressText: "coming soon",
   },
@@ -45,6 +53,7 @@ const cities = [
     id: 6,
     name: "Berlin",
     imageSrc: "assets/Berlin.png",
+    href: "",
     imageAlt: "Berlin city",
     progressText: "coming soon",
   },
@@ -83,25 +92,27 @@ export default function Cities() {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/mapbox/outdoors-v11",
+      style: "mapbox://styles/mapbox/light-v10",
       center: [lng, lat],
       zoom: zoom,
     });
 
-    new mapboxgl.Marker().setLngLat([-4.2568, 55.8508]).addTo(map.current); //Glasgow
-    new mapboxgl.Marker({ color: "black" })
+    new mapboxgl.Marker({ color: "#1d6e8e" })
+      .setLngLat([-4.2568, 55.8508])
+      .addTo(map.current); //Glasgow
+    new mapboxgl.Marker({ color: "#9CA3AF" })
       .setLngLat([-0.1276, 51.5072])
       .addTo(map.current); //London
-    new mapboxgl.Marker({ color: "black" })
+    new mapboxgl.Marker({ color: "#9CA3AF" })
       .setLngLat([-6.2603, 53.3498])
       .addTo(map.current); //Dublin
-    new mapboxgl.Marker({ color: "black" })
+    new mapboxgl.Marker({ color: "#9CA3AF" })
       .setLngLat([2.3522, 48.8566])
       .addTo(map.current); //Paris
-    new mapboxgl.Marker({ color: "black" })
+    new mapboxgl.Marker({ color: "#9CA3AF" })
       .setLngLat([9.19, 45.4642])
       .addTo(map.current); //Milan
-    new mapboxgl.Marker({ color: "black" })
+    new mapboxgl.Marker({ color: "#9CA3AF" })
       .setLngLat([13.405, 52.52])
       .addTo(map.current); //Berlin
 
@@ -112,9 +123,9 @@ export default function Cities() {
   return (
     <>
       <NavBar current="invest" />
-      <div className="bg-gray py-16 sm:py-24 lg:py-20 px-16">
+      <div className="bg-white">
         <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4">
-          <div className="mt-10">
+          <div className="mt-10 py-16 sm:py-24 lg:py-20 px-16">
             <h2 className="font-grotesk mt-2 lg:text-5xl md:text-4xl text-gray-900 sm:text-4xl">
               Find places to invest in nature
             </h2>
@@ -122,19 +133,24 @@ export default function Cities() {
               Discover portfolios of projects or individual actions that can
               reduce your carbon footprint and climate-related liabilities.
             </p>
-            <p className="mt-3 text-lg font-semibold leading-6 text-gray-500">
+            <p className="mt-3 text-lg font-semibold leading-6 text-blue4">
               Select a city of interest to find our more
             </p>
             <div className="mt-5">
               <h3 className="text-l text-gray-900 sm:text-xl py-5">Cities</h3>
-              <div className="grid gap-y-10 gap-x-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+              <div className="grid gap-y-10 gap-x-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
                 {cities.map((city) => (
                   <div key={city.id} className="group relative">
                     <div className="">
                       <img
                         src={city.imageSrc}
                         alt={city.imageAlt}
-                        className=""
+                        className={classNames(
+                          city.progressText === "coming soon"
+                            ? "image-blur"
+                            : "border-2 rounded-md border-blue4 ",
+                          ""
+                        )}
                       />
                     </div>
                     <div className="mt-0 flex justify-between">
@@ -162,22 +178,22 @@ export default function Cities() {
             <div ref={mapContainer} className="map-container" />
           </div>
         </div>
-        <div className="bg-gray">
+        <div className="bg-white">
           <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-            <div className="bg-indigo-700 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
+            <div className="bg-green4 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
               <div className="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
                 <div className="lg:self-center">
                   <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
                     <span className="block">Try becoming a nature steward</span>
                   </h2>
-                  <p className="mt-4 text-lg leading-6 text-indigo-200">
+                  <p className="mt-4 text-lg leading-6 text-white">
                     Cover up to 10% of your maintenance costs by preserving
                     street trees, rewilding your local park or replenishing the
                     urban forest.
                   </p>
                   <Link
                     to="/become-a-steward"
-                    className="mt-8 bg-gray border border-transparent rounded-md shadow px-5 py-3 inline-flex items-center text-base font-medium text-indigo-600 hover:bg-indigo-50"
+                    className="mt-8 bg-gray border border-transparent rounded-md shadow px-5 py-3 inline-flex items-center text-base font-medium text-green4 hover:bg-indigo-50"
                   >
                     Learn more
                   </Link>
@@ -206,7 +222,6 @@ export default function Cities() {
                       {faq.answer}
                     </dd>
                     <Link
-                      type="button"
                       to="/faq"
                       className="inline-flex items-center px-4 py-2 mt-4 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
