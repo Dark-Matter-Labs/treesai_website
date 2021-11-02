ee3579dfbd207751759da88484721a0d515917bf
axeln78 <axeln78@gmail.com>
2021-11-02 17:40:28 +0100

    Added Dummy data to the portfolio. The data is based on 1000 trees with 20 cm of avg DBH

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,6 +6,37 @@ import { XIcon } from "@heroicons/react/outline";
 
 import NavBar from "../components/demo/NavBarGlasgow";
 
+const mock_seq_arr = [
+  11.30873456069503, 11.821495350815411, 12.35200886155292, 12.900619162733761,
+  13.46766826884179, 14.053496013188948, 14.658439930993453, 15.282835151096904,
+  15.927014296039076, 16.59130739019575, 17.276041775677683, 17.98154203568467,
+  18.708129925002307, 19.456124307331436, 20.225841099137796,
+  21.017593219713884, 21.831690547147815, 22.66843987989867, 23.528144903686098,
+  24.411106163402962, 25.317621039775133, 26.247983730495292,
+  27.202485235567735, 28.181413346608537, 29.18505263986074, 30.213684472683273,
+  31.267586983292006, 32.34703509353443, 33.45230051449016, 34.58365175470226,
+  35.74135413084781, 36.925669780668784, 38.13685767799598, 39.37517364969739,
+  40.64087039440528, 41.93419750286898, 43.25540147980524, 44.60472576709907,
+  45.9824107682598, 47.38869387398506, 48.82380948875107, 50.287989058308256,
+  51.781461097994566, 53.30445122177889, 54.8571821719388, 56.43987384930426,
+  58.05274334398689, 59.69600496653004, 61.36987027940768, 63.0745481288194,
+];
+const mock_water_arr = [
+  310.03485139283066, 312.1493821239187, 314.25394721807925, 316.3481427154397,
+  318.43159496390564, 320.50395941911376, 322.56491944185285,
+  324.61418509947237, 326.6514919770009, 328.67660000296416, 330.68929229420104,
+  332.6893740233478, 334.6766713120741, 336.65103015262656, 338.61231535975384,
+  340.56040955465096, 342.49521218217114, 344.4166385622061, 346.3246189758235,
+  348.2190977864824, 350.10003259640393, 351.96739343796924, 353.82116199983886,
+  355.6613308873303, 357.4879029164643, 359.30089044098077, 361.1003147115341,
+  362.88620526620815, 364.6585993514307, 366.4175413723263, 368.16308237151065,
+  369.89527953531393, 371.6141957263997, 373.3198990417512, 375.0124623949902,
+  376.6919631220055, 378.35848260888383, 380.01210594114593, 381.6529215733167,
+  383.281021017881, 384.89649855269744, 386.49945094597655, 388.0899771979537,
+  389.6681782984151, 391.23415699927307, 392.78801760140834, 394.3298657550321,
+  395.8598082728519, 397.37795295535346, 398.88440842754113,
+];
+
 const projects = [
   {
     id: 1,
@@ -72,7 +103,7 @@ const carbon_data = {
   ],
   datasets: [
     {
-      data: [9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42],
+      data: mock_seq_arr.slice(0, 10),
       backgroundColor: ["#10B981"],
       borderColor: ["#10B981"],
       borderWidth: 1,
@@ -120,7 +151,7 @@ const stormwater_data = {
   ],
   datasets: [
     {
-      data: [9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42],
+      data: mock_water_arr.slice(0, 10),
       backgroundColor: ["#3B82F6"],
       borderColor: ["#3B82F6"],
       borderWidth: 1,
@@ -168,12 +199,17 @@ const faqs = [
 
 export default function Portfolio() {
   mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
-
+  //Map related variables
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng] = useState(-4.2518);
   const [lat] = useState(55.8642);
   const [zoom] = useState(11);
+
+  // ES related variables
+  // uncomment those spooky lines to get the yearly average
+  //console.log(mock_seq_arr.slice(0, 10).reduce((a, b) => a + b, 0)/10);
+  //console.log(mock_water_arr.slice(0, 10).reduce((a, b) => a + b, 0)/10);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -242,7 +278,7 @@ export default function Portfolio() {
               </dt>
               <dd className="ml-2 pb-6 flex items-baseline sm:pb-7">
                 <p className="text-2xl font-semibold text-green-600 ">
-                  800 tCO2e
+                  13.83 tCO2e
                 </p>
                 <p className="text-gray-900 ml-2 flex items-baseline text-sm font-semibold">
                   per year
@@ -256,7 +292,7 @@ export default function Portfolio() {
                 </p>
               </dt>
               <dd className="ml-2 pb-6 flex items-baseline sm:pb-7">
-                <p className="text-2xl font-semibold text-blue2 ">27,300m³</p>
+                <p className="text-2xl font-semibold text-blue2 ">319.4m³</p>
                 <p className="text-gray-900 ml-2 flex items-baseline text-sm font-semibold">
                   per year
                 </p>
