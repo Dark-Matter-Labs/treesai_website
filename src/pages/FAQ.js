c312d62e69e46f034a24c4c5bdfa56f8509a94b0
Gurden Batra <gurden.batra@gmail.com>
2021-11-04 20:08:19 +0200

    Update FAQ.js

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -135,7 +135,7 @@ export default function FAQ() {
       <NavBar current="faq" />
       <div className="bg-white">
         <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-          <div className="max-w-2xl lg:mx-auto">
+          <div className="">
             <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
               TreesAI FAQ
             </h2>
