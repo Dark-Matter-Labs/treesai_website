f094f10d2d2fccadb8a88ca288898ea94a0e90da
Gurden Batra <gurden.batra@gmail.com>
2022-05-04 16:18:28 +0300

    Adding test component to embed ArcGIS map

---
+++ src/pages/GISMap.js
@@ -0,0 +1,9 @@
+
+
+export default function GISMap() {
+  return (
+      <div>
+          <iframe title="Glasgow Portfolio 01" class="frame-border" src="https://darkmatter.maps.arcgis.com/apps/instant/basic/index.html?appid=235cd2f222154548990c426d053fc28d" width="400" height="600" frameBorder="0" allowFullScreen>iFrames are not supported on this page.</iframe>
+      </div>
+  );
+}
