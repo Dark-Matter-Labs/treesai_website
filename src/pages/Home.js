e2a343eefe6fcbf700c3b9f899f5e3e836c6160d
Gurden Batra <gurden.batra@gmail.com>
2023-01-12 18:38:18 +0200

    remove old components from home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,16 @@
-import Vision from "../components/Vision";
-import ValueProp from "../components/ValueProp";
-import Features from "../components/Features";
-import HowItWorks from "../components/HowItWorks";
-import SocialProof from "../components/SocialProof";
-import Pilot from "../components/Pilot";
-import Team from "../components/Team";
-import Reports from "../components/Reports";
-import GetInTouch from "../components/GetInTouch";
-import Footer from "../components/Footer";
+import Vision from '../components/Vision';
+import ValueProp from '../components/ValueProp';
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
 
 export default function Home() {
   return (
-    <div className="bg-white">
-      <div className="relative overflow-hidden">
+    <div className='font-favorit bg-white-200'>
+      <div className='relative overflow-hidden'>
+        <NavBar />
         <main>
           <Vision />
           <ValueProp />
-          <Features />
-          <HowItWorks />
-          <Pilot />
-          <SocialProof />
-          <Reports />
-          <Team />
-          <GetInTouch />
           <Footer />
         </main>
       </div>
