799351b25786b405edbfb1210b9196440286e67e
Gurden Batra <gurden.batra@gmail.com>
2023-01-12 18:36:48 +0200

    Prettier fix

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import Privacy from "../components/Privacy";
-import Footer from "../components/Footer";
+import Privacy from '../components/Privacy';
+import Footer from '../components/Footer';
 
 export default function PrivacyPolicy() {
   return (
-    <div className="bg-gray">
-      <div className="relative overflow-hidden">
-        <div className="mt-10 space-x-8 md:flex md:ml-10">
+    <div className='bg-gray'>
+      <div className='relative overflow-hidden'>
+        <div className='mt-10 space-x-8 md:flex md:ml-10'>
           <Link
-            to="/"
-            className="text-base font-medium text-dark hover:text-gray-300 pointer-cursor"
+            to='/'
+            className='text-base font-medium text-dark hover:text-gray-300 pointer-cursor'
           >
             ← Back
           </Link>
